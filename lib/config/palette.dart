fe12319566376a7f36c1e69298a9138ef856e6ea
akaseoyoung <think000412@gmail.com>
2023-05-18 16:23:43 +0900

    working on main view screen ui

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,4 +2,9 @@ import 'package:flutter/painting.dart';
 
 class Palette{
   static const Color defaultColor = Color(0xFF000000);
+  static const Color squareColor = Color(0xFFE4EBF6);
+  static const Color buttonColor = Color(0xFFD9D9D9);
+  static const Color buttonIconColor = Color(0xFF868686);
+  static const Color LikedColor = Color(0xFFFFB4B4);
+  static const Color StarColor = Color(0xFFFFEE95);
 }
--- lib/screens/view.dart
+++ lib/screens/view.dart
@@ -9,11 +9,76 @@ class ViewScreen extends StatefulWidget {
 }
 
 class _ViewScreenState extends State<ViewScreen> {
-  bool isViewSc = true;
+  bool isViewScreen = true;
+  bool isSelected = true;  // default : not liked
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Palette.defaultColor
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0), // remove appbar
+        child: AppBar(
+          title: Text(''),
+          backgroundColor: Colors.black,
+          elevation: 0.0,
+        ),
+      ),
+      body: Center( // align the widgets to center
+        child: Column( // make a vertical array of widgets
+          children: [
+              SizedBox(
+                height: 180,
+              ),
+              Expanded( // fit size to the screen
+                child: GridView.builder(
+                itemCount: 9,
+                gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1/1,
+                ),
+                itemBuilder: (BuildContext context, int index){
+                  return Stack(
+                    children: <Widget>[
+                      Container(
+                        color: Color(0xFF9CABC2).withOpacity(0.35),
+                        margin: EdgeInsets.all(1), // space between each squares
+                        child: Center(  // centered top-bottom
+                          child: Text(
+                            '식당$index',
+                            textAlign: TextAlign.center, //centered left-right
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontSize: 20,
+                              color: Palette.defaultColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        width: 200,
+                        height: 10,
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isSelected = !isSelected;
+                            });
+                          },
+                          color: Palette.LikedColor,
+                          icon: Icon(
+                            isSelected ? Icons.favorite
+                                : Icons.favorite_border,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
